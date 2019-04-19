c469796483d9f2145ab991c2bb91dc99103c5a6c
Max Fenig <mfenig1013@users.noreply.github.com>
2019-04-19 11:41:38 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dautils',
+      version='0.1',
+      description='a few data analysis tools',
+      url='http://github.com/mfenig1013/dautils',
+      author='Max Fenig',
+      author_email='mfenig1013@gmail.com',
+      license='MIT',
+      packages=['dautils'],
+      zip_safe=False)
